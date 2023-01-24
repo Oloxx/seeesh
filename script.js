2cd9210b8b4e412a186bc5f9ba4653bb69bc6783
Oloxx <alexrodart03@gmail.com>
2023-01-24 17:08:25 +0100

    Colors added

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ function playSound(audio) {
   sound.volume = 0.25;
   sound.play();
 }
+
+function random() {
+  const array = ['btn-outline-primary', 'btn-outline-success', 'btn-outline-info', 'btn-outline-danger'];
+  const random = Math.floor(Math.random() * array.length);
+  const result = array[random];
+
+  var elements = document.getElementsByTagName('button');
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].classList.add(result);
+  }
+}
