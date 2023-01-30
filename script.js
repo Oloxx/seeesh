f534f8eb48f0447c0ca020e93183d6e4e6d15a16
Oloxx <alexrodart03@gmail.com>
2023-01-30 15:53:23 +0100

    New colors & new sound

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function playSound(audio) {
 }
 
 function random() {
-  const array = ['btn-outline-primary', 'btn-outline-success', 'btn-outline-info', 'btn-outline-danger'];
+  const array = ['btn-outline-info', 'btn-outline-danger', 'btn-outline-lime', 'btn-outline-warning'];
   const random = Math.floor(Math.random() * array.length);
   const result = array[random];
 
