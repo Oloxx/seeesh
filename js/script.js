2814c1457ce08f93440b83749f963f3514f57b3e
Oloxx <alexrodart03@gmail.com>
2023-02-06 18:36:06 +0100

    Added purple color

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function playSound(audio) {
 }
 
 function random() {
-  const array = ['btn-outline-info', 'btn-outline-danger', 'btn-outline-lime', 'btn-outline-warning'];
+  const array = ['btn-outline-info', 'btn-outline-danger', 'btn-outline-lime', 'btn-outline-warning', 'btn-outline-purple'];
   const random = Math.floor(Math.random() * array.length);
   const result = array[random];
 
